4219cc4a6bdea4d0d629f55ed0a8778cd79938d2
JAFARMAKANDAR <jafarmakandar991@gmail.com>
2024-06-22 14:04:36 +0530

    first commit

---
+++ stagano_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StaganoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stagano_app'
---
+++ stagano_app/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+import stepic #pip install stepic
+# Create your views here.
