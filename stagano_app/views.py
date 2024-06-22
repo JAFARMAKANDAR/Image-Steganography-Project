1aff88c45df671b023cb2810033267c1656a2f89
JAFARMAKANDAR <jafarmakandar991@gmail.com>
2024-06-22 15:46:31 +0530

    created pages of navbar

--- stagano_app/views.py
+++ stagano_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-import stepic #pip install stepic
+from PIL import Image  #pip install Pillow
+import stepic    #pip install stepic
+
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def encryption_view(request):
+    return render(request,'encryption.html')
+
+def decryption_view(request):
+    return render(request,'decryption.html')
--- stegano_project/settings.py
+++ stegano_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stegano_app',
+    'stagano_app',
 ]
 
 MIDDLEWARE = [
